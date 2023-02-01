8448f05ce0002976ae49c0d056cd56155778cdfd
Luis Miguel Banda Montes <lbanda@choiceworx.com>
2023-02-01 12:19:56 -0600

    Adding dynamic component alert using ngIf and programatically with view container reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     DropdownDirective,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,32 @@
-import { Component } from "@angular/core"
+import { Component, OnDestroy, ViewChild, ViewContainerRef } from "@angular/core"
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent  implements OnDestroy{
+    //@ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+    private closeSub : Subscription;
+
     isLoginMode = true;
     isLoading = false;
     error:string = null;
 
     constructor(private authService:AuthService,
-        private router: Router) {
+        private router: Router,
+        private viewContainerRef: ViewContainerRef) {
 
+    }
+    ngOnDestroy(): void {
+       if(this.closeSub){
+        this.closeSub.unsubscribe();
+       }
     }
 
     onSwitchMode(){
@@ -45,13 +56,28 @@ export class AuthComponent {
                 this.isLoading = false;
                 this.router.navigate(['/recipes']);
             },
-            error: (errorMessage) =>{
-                this.error = errorMessage;                    
-                console.log(errorMessage);
+            error: (error) =>{
+                this.error = error.message;                    
+                console.log(error);
+                this.showErrorAlert(error.message);
                 this.isLoading = false;
             }
         });
 
         authForm.reset();
+    }
+
+    onHandleError(){
+        this.error = null;
+    }
+
+    private showErrorAlert(message: string){
+        const alertComponentRef = this.viewContainerRef.createComponent(AlertComponent);
+        alertComponentRef.setInput("message", message);
+        alertComponentRef.instance.message = message;
+        this.closeSub = alertComponentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            this.viewContainerRef.clear();
+        });
     }
 }
