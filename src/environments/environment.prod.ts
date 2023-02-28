979d36da9ee00d0eeac2650ad7429c7fd0d95501
Luis Miguel Banda Montes <lbanda@choiceworx.com>
2023-02-27 22:22:56 -0600

    Add setting to use environment setups and required libraries

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    firebaseApiKey: 'AIzaSyBOvt1oIDtpqRWWqM6PMy2vFW7eiXOJmY0',
+    firebaseConfig: {
+        apiKey: "AIzaSyBOvt1oIDtpqRWWqM6PMy2vFW7eiXOJmY0",
+        authDomain: "ng-course-recipe-book-f8b4c.firebaseapp.com",
+        databaseURL: "https://ng-course-recipe-book-f8b4c-default-rtdb.firebaseio.com",
+        projectId: "ng-course-recipe-book-f8b4c",
+        storageBucket: "ng-course-recipe-book-f8b4c.appspot.com",
+        messagingSenderId: "213210311077",
+        appId: "1:213210311077:web:8b588785c0081f3cdf604c",
+        measurementId: "G-Z0722CY2D8"
+    }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseApiKey: 'AIzaSyBOvt1oIDtpqRWWqM6PMy2vFW7eiXOJmY0',
+    firebaseConfig: {
+        apiKey: "AIzaSyBOvt1oIDtpqRWWqM6PMy2vFW7eiXOJmY0",
+        authDomain: "ng-course-recipe-book-f8b4c.firebaseapp.com",
+        databaseURL: "https://ng-course-recipe-book-f8b4c-default-rtdb.firebaseio.com",
+        projectId: "ng-course-recipe-book-f8b4c",
+        storageBucket: "ng-course-recipe-book-f8b4c.appspot.com",
+        messagingSenderId: "213210311077"      
+    }
+}
